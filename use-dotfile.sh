b3f515d6cdff48e4a57f7a5a7a3731c45a972c52
basi-a <basi-a@outlook.com>
2023-01-29 19:33:42 +0800

    update script

--- use-dotfile.sh
+++ use-dotfile.sh
@@ -3,6 +3,8 @@ if [ ! -d "${HOME}/.aria2" ];then
     mkdir ${HOME}/.aria2
     touch ${HOME}/.aria2/aria2.session
 fi
-sed -i "s|/home/---|${HOME}|g" aria2.conf.example
 config_file="`pwd`/aria2.conf.example"
+
+sed -i "s|/home/---|${HOME}|g" ${config_file}
+
 ln -sf ${config_file} ${HOME}/.aria2/aria2.conf
