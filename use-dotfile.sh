c3add1d6296c9b8673fe36adc9c4b4080f674da6
basi-a <basi-a@outlook.com>
2023-01-29 18:04:30 +0800

    update script

--- use-dotfile.sh
+++ use-dotfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -d "$HOME/.aria2c" ];then
-    mkdir $HOME/.aria2c
+if [ ! -d "$HOME/.config/aria2c" ];then
+    mkdir $HOME/.config/aria2c
 fi
 config_file="`pwd`/aria2c.conf.example"
-ln -sf ${config_file} $HOME/.aria2c/aria2c.conf
+ln -sf ${config_file} $HOME/.config/aria2c/aria2c.conf
