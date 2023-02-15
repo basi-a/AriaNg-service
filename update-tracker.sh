4593c7e4d3fd7efecde70c9ee4f0cacbea20e8cc
basi-a <basi-a@outlook.com>
2023-02-15 14:35:49 +0800

    update url

--- update-tracker.sh
+++ update-tracker.sh
@@ -2,6 +2,6 @@
 if [ -f "best_aria2.txt" ];then
     rm best_aria2.txt
 fi
-wget https://trackerslist.com/best_aria2.txt -q --show-progress
+wget https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best_aria2.txt -q --show-progress
 list="`cat best_aria2.txt`"
 sed -i "s\bt-tracker=\bt-tracker=${list}\g" aria2.conf.example
